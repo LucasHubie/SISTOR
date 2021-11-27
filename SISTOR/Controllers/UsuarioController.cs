6fe9e95704340ded97732227b170012a9139f4f3
Mateus Bueno <bueno.030702@gmail.com>
2021-11-27 13:26:52 -0300

    check in inicial bueno

---
+++ SISTOR/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SISTOR.Configuration;
+using SISTOR.Interfaces;
+using SISTOR.Models;
+using SISTOR.Repository;
+using SISTOR.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISTOR.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly AppDbContext _context;
+        private IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(AppDbContext context, IUsuarioRepositorio usuarioRepositorio)
+        {
+            _context = context;
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(Usuario user)
+        {
+            _usuarioRepositorio.CriarUsuario(user);
+            return Json(new { user = user});
+        }
+    }
+}
---
+++ SISTOR/Repository/UsuarioRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using SISTOR.Configuration;
+using SISTOR.Controllers;
+using SISTOR.Interfaces;
+using SISTOR.Models;
+using SISTOR.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISTOR.Repository
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly AppDbContext _context;
+        public UsuarioRepositorio(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        public AppDbContext AppDbContext
+        {
+            get { return _context as AppDbContext; }
+        }
+
+        public Usuario CriarUsuario(Usuario user)
+        {
+            if(user != null)
+            {
+                user.Senha = Md5Hash.CalculaHash(user.Senha);
+                _context.Add(user);
+                _context.SaveChanges();
+            }
+            return user;
+        }
+
+    }
+}
