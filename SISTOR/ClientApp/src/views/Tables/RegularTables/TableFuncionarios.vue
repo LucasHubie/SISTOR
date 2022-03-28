b5d8641b2bfb79e02dcecd28ce7762c0285b42bd
Lucas Hubie <lucas_hubie@hotmail.com>
2022-03-28 19:00:49 -0300

    Revert "funcionarios validacao form"
    This reverts commit 389577f06fe989f0caef47ca00a4a04e8dec32b7.

--- SISTOR/ClientApp/src/views/Tables/RegularTables/TableFuncionarios.vue
+++ SISTOR/ClientApp/src/views/Tables/RegularTables/TableFuncionarios.vue
@@ -335,7 +335,7 @@
         <template #modal-footer>
           <b-row>
             <b-col lg="12">
-              <base-button type="success" class="float-right" style="margin-right: 10px;" v-on:click="handleOk()">
+              <base-button type="success" class="float-right" style="margin-right: 10px;" v-on:click="sendForm()">
                 <span class="btn-inner--text">Confirmar</span>
                 
               </base-button>
@@ -715,11 +715,11 @@
         this.tpOperacao = 'Incluir'
       },
       tipoPessoa() {
-          this.funcionario.pessoa.tipoPessoa = 1
+          this.Funcionario.Pessoa.TipoPessoa = 1
       },
       handleOk(bvModalEvt) {
         // Prevent modal from closing
-        //bvModalEvt.preventDefault()
+        bvModalEvt.preventDefault()
         // chama função tipoPessoa
         this.tipoPessoa()
         // Trigger submit handler
